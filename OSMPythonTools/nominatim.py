2a8b62d0931a6ecd17fbf71271cc949943567df6
Arun Drelich <arun_drelich@brown.edu>
2018-08-16 15:20:32 -0400

    fix dict mutation bug that was affecting hashing

--- OSMPythonTools/nominatim.py
+++ OSMPythonTools/nominatim.py
@@ -6,20 +6,19 @@ class Nominatim(CacheObject):
     def __init__(self, endpoint='https://nominatim.openstreetmap.org/search', params={}, **kwargs):
         super().__init__('nominatim', endpoint, **kwargs)
         self._params = params
-    
+
     def _queryString(self, query, wkt=False, **kwargs):
         params = kwargs['params'] if 'params' in kwargs else {}
         if wkt:
             params['polygon_text'] = '1'
         return (query, query, params)
-    
-    def _queryRequest(self, endpoint, queryString, params={}):
+
+    def _queryRequest(self, endpoint, queryString, params=None):
         if not params:
             params = {}
-        params['format'] = 'json'
-        params['q'] = queryString
-        return endpoint + '?' + urllib.parse.urlencode(params)
-    
+        paramsDict = {'format':'json', 'q':queryString, **params}
+        return "{}?{}".format(endpoint, urllib.parse.urlencode(paramsDict))
+
     def _rawToResult(self, data, queryString):
         return NominatimResult(data, queryString)
 
@@ -27,13 +26,13 @@ class NominatimResult:
     def __init__(self, json, queryString):
         self._json = json
         self._queryString = queryString
-    
+
     def toJSON(self):
         return self._json
-    
+
     def queryString(self):
         return self._queryString
-    
+
     def areaId(self):
         for d in self._json:
             if 'osm_type' in d and d['osm_type'] == 'relation' and 'osm_id' in d:
